52c0b0827373a639a85d11216c5903680021e66a
karan-td7 <mkaran71999@gmail.com>
2024-07-09 00:52:22 +0530

    form

---
+++ src/pages/tylerDurden/form/index.tsx
@@ -0,0 +1,14 @@
+import Breadcrumb from "../../../components/Breadcrumbs/Breadcrumb";
+import UserForm from "../../../components/tylerDurden/form";
+
+const Form = () => {
+  return (
+    <>
+      <Breadcrumb pageName="Form" />
+
+      <UserForm />
+    </>
+  );
+};
+
+export default Form;
